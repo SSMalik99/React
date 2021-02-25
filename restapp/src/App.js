6b54b8a926dc6cb9515f6eec99457d064bd206b5
SSMalik99 <saravjeetsingh.malik@gmail.com>
2021-02-26 03:54:23 +0530

    F&&&

---
+++ restapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import logo from './logo.svg';
+import './App.css';
+import Routing from './Component/routing'
+
+function App() {
+  return (
+    <>
+    <Routing/>
+    </>
+  );
+}
+
+export default App;
